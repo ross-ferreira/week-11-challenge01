f1c316f4fb4b6408fed4bac045e5605086c77fc8
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-04-09 10:42:17 +0100

    added deploy reqs

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import {
 //   BrowserRouter as Router, Route, Switch,} from "react-router-dom"; 
-import {Router,Route} from "react-router-dom"; 
+import { HashRouter as Router, Route,Switch } from "react-router-dom";
 import Header from "./Header";
 import WinnerBoard from './WinnerBoard'
 import Button from './Button'
