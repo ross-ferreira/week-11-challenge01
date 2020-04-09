225fe294c8a16d1de70081af4e3921d1ce1604d9
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-04-09 10:42:51 +0100

    Updates

---
+++ precache-manifest.8f449ab9829c627cb7ef706ca7293b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2368c350bcdf1e6e70ccdf7e50fdac",
+    "url": "/week-11-challenge01/index.html"
+  },
+  {
+    "revision": "0e20759d63dcdd1a1cb3",
+    "url": "/week-11-challenge01/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0dd9c9a6be88bef140f5",
+    "url": "/week-11-challenge01/static/js/2.161df38c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/week-11-challenge01/static/js/2.161df38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e20759d63dcdd1a1cb3",
+    "url": "/week-11-challenge01/static/js/main.946281e2.chunk.js"
+  },
+  {
+    "revision": "603e593dd7d6fc4e7db5",
+    "url": "/week-11-challenge01/static/js/runtime-main.6dee92c1.js"
+  }
+]);
